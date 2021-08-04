6174429d7f62f343d695f29595f1bf04cf58aecc
Dan O'Leary <olearydaniel@me.com>
2021-08-04 09:47:15 -0500

    Further UIKit integration.
    Error in landscape display of background image.

---
+++ ARMoveAndMatch/AR CoachingView/FocusSquare Readme.md
@@ -0,0 +1,7 @@
+
+
+
+FocusSquare illustrates the surface which the AR Scene will be placed. This area of code has been taken from Apple's own sample code at the following address.
+
+https://developer.apple.com/documentation/arkit/environmental_analysis/placing_objects_and_handling_3d_interaction
+
--- ARMoveAndMatch/ViewController.swift
+++ ARMoveAndMatch/ViewController.swift
@@ -15,10 +15,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let scene = try! Experience.loadLetters()
+
+		// Hard coded letters logic for demo purposes.
+
+		// Allow for gesture control in the scene.
+		guard let A = scene.uppercaseA else { return }
+		if let uppercaseA = A as? Entity & HasCollision {
+			uppercaseA.generateCollisionShapes(recursive: true)
+//			arView.installGestures([.rotation, .translation], for: uppercaseA)
+		}
+
+
+		guard let B = scene.uppercaseB else { return }
+		if let uppercaseB = B as? Entity & HasCollision {
+			uppercaseB.generateCollisionShapes(recursive: true)
+//			arView.installGestures([.rotation, .translation], for: uppercaseB)
+		}
+
+
+		guard let C = scene.uppercaseC else { return }
+		if let uppercaseC = C as? Entity & HasCollision {
+			uppercaseC.generateCollisionShapes(recursive: true)
+//			arView.installGestures([.rotation, .translation], for: uppercaseC)
+		}
+
+
+		guard let D = scene.uppercaseD else { return }
+
+		if let uppercaseD = D as? Entity & HasCollision {
+			uppercaseD.generateCollisionShapes(recursive: true)
+//			arView.installGestures([.rotation, .translation], for: uppercaseD)
+		}
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(scene)
     }
+
+
 }
